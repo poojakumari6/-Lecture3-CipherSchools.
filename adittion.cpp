a45e72de502dea913853d674cb35b6f851a1ffea
poojakumari6 <137697560+poojakumari6@users.noreply.github.com>
2023-06-28 19:27:26 +0530

    Create adittion.cpp

---
+++ adittion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int c;
+	//declare two variables and then asign them values of 3,5 and then add them
+	
+	 a=3;
+	 b=5;
+	
+	
+	cout<<a+b<<endl<<"a+b";
+	
+	
+	return 0;
+	
+}
